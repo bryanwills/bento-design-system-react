9b9b8f0bce28851f1a2bb80510e853d88e484c37
Vincenzo Guerrisi <vincenzo@buildo.io>
2024-03-15 11:24:13 +0100

    Fix Columns layout to respect space between columns

--- packages/bento-design-system/src/Layout/Columns.tsx
+++ packages/bento-design-system/src/Layout/Columns.tsx
@@ -9,7 +9,17 @@ import {
   CollapsibleAlignmentProps,
   responsiveCollapsibleAlignmentProps,
 } from "../util/collapsible";
-import { wideWidths, desktopWidths, tabletWidths, mobileWidths, fullWidth } from "./Column.css";
+import {
+  wideWidths,
+  desktopWidths,
+  tabletWidths,
+  mobileWidths,
+  fullWidth,
+  columnsSpace,
+  column,
+  columnContent,
+} from "./Column.css";
+import { assignInlineVars } from "@vanilla-extract/dynamic";
 
 type ColumnProps = {
   children: Children;
@@ -26,8 +36,9 @@ export function Column({ children, width, sticky }: ColumnProps) {
 
   const className =
     width == null
-      ? fullWidth
+      ? [column, fullWidth]
       : [
+          column,
           wide && wideWidths[wide],
           desktop && desktopWidths[desktop],
           tablet && tabletWidths[tablet],
@@ -38,7 +49,7 @@ export function Column({ children, width, sticky }: ColumnProps) {
 
   return (
     <Box className={className} {...stickyProps}>
-      {children}
+      <Box className={columnContent}>{children}</Box>
     </Box>
   );
 }
@@ -52,8 +63,8 @@ export function Columns({ space, children, align, alignY, collapseBelow, reverse
   return (
     <Box
       display="flex"
-      gap={space}
       {...responsiveCollapsibleAlignmentProps({ align, alignY, collapseBelow, reverse })}
+      style={assignInlineVars({ [columnsSpace]: `${space}px` })}
     >
       {flattenChildren(children).map((child, index) => {
         if (isColumn(child)) {
